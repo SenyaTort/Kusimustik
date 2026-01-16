721bb7647473dec506424efd9e855dc955cacb75
opilane <opilane@HP-CZC2349HV0>
2026-01-16 13:54:39 +0200

    Add project files.

---
+++ Küsimustliku.py
@@ -0,0 +1,23 @@
+import smtplib, ssl
+from email.message import EmailMessage
+def andmete_lugemine_failidest():
+    pass
+
+def andmete_salvestamine_failidesse():
+    nküs=input("Sisesta teie küsimus: ")
+    nvas=input("Sisesta vastus selle küsimus: ")
+
+def testimine():
+    testitud=[]
+    nimi2=input("Sisesta oma nimi: ")
+    if nimi2=testitud:
+        print("See nini on testitud")
+    else:
+        nimi.append(nimi2)
+    pass
+
+def email_saatmine():
+    pass
+
+def küsimuste_lisamine():
+    pass
